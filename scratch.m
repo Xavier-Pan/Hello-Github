5d672ad75c0ccc3986a986012b58cc50cdf81c71
Xavier-Pan <sypan.cser@gmail.com>
2017-03-13 16:11:48 +0800

    Add files via upload
    log少負號

--- scratch.m
+++ scratch.m
@@ -15,9 +15,17 @@
 %    fprintf('in i=%d',i);
 %end
 %===gradient check
+W{1}=rand(10,784);
+W{1} = reshape(sin(1:numel(W{1})), size(W{1})) / 10;
+W{2}=rand(10,10);
+W{3}=rand(10,10);
+W{4}=rand(10,10);
+b=rand(10,4);
+
 hiddLayerNum=3;hiddlayerNum=3;
-delta=10^-10;
-  [output,h]=fordProp(hiddLayerNum,W,b,test_x(1,:));% h include all activation layer,h(:,end)=estimate_y
+delta=10^-4;
+
+[output,h]=fordProp(hiddLayerNum,W,b,test_x(1,:));% h include all activation layer,h(:,end)=estimate_y
 [t_grad_W,t_grad_b]=backProp(test_x(1,:),h,W,test_label(1,:));
 %===numarical gradient====
 num_W=W;
@@ -26,13 +34,13 @@ for k=1:size(W,2)
         for j=1:size(W{k},2)
             delta1_W=W;
             delta2_W=W;
-            delta1_W{k}(i,j)=delta1_W{k}(i,j)+delta;
-            delta2_W{k}(i,j)=delta2_W{k}(i,j)-delta;
+            delta1_W{k}(i,j)=W{k}(i,j)+delta;
+            delta2_W{k}(i,j)=W{k}(i,j)-delta;
             %==compute J ===
             y=fordProp(hiddlayerNum,delta1_W,b,test_x(1,:));
-            delta_J1=log(y)'*test_label(1,:)';
+            delta_J1=-log(y)'*test_label(1,:)';
             y=fordProp(hiddlayerNum,delta2_W,b,test_x(1,:));
-            delta_J2=log(y)'*test_label(1,:)';
+            delta_J2=-log(y)'*test_label(1,:)';
             %====
             num_W{k}(i,j)=(delta_J1-delta_J2)/(2*delta);
         end
