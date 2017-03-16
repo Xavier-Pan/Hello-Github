069d4f78de38850ffaba88d6bc7d9f85519d065f
Xavier-Pan <sypan.cser@gmail.com>
2017-03-16 23:02:05 +0800

    Add files via upload
    previous version is ERROR!!!!!!!!!!!!!!!!!!!!!!!!!
    softmax only in the last layer,not every hidden layer

--- costf_matrixVersion.m
+++ costf_matrixVersion.m
@@ -5,10 +5,7 @@ function J=costf_matrixVersion(X,W,b,labels,hiddLayerNum)
 %==================================================    
    X=X';%each column is a data
    N=size(X,2);
-   H{1}=sigmoid(bsxfun(@plus,b{1},W{1}*X));   
-   for i=2:hiddLayerNum+1
-       H{i}=softmax(bsxfun(@plus,b{i},W{i}*H{i-1})); 
-   end   
+   H=fordProp_batchVersion(W,b,X,hiddentLayerNum)  
    S=log(H{end})'.*labels(1:size(H{end},2),:);% each row is cross entropy for a data
    J=ones(1,size(S,1))*S*ones(size(S,2),1);%sum all the term in S
    J=-J/N;
