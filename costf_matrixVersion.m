e1bc20655c1be0a5c42611f79dbccd4d57723701
Xavier-Pan <sypan.cser@gmail.com>
2017-03-16 23:39:59 +0800

    Add files via upload
    previous version is ERROR again!!!!!!!!!!!!!!!!
    X'row must be a data
    so X=X' in costf_matrixVersion() need after call fordProp()

--- costf_matrixVersion.m
+++ costf_matrixVersion.m
@@ -3,9 +3,10 @@ function J=costf_matrixVersion(X,W,b,labels,hiddLayerNum)
 % b:bias  
 % W:weight matrix
 %==================================================    
+   H=fordProp_batchVersion(W,b,X,hiddLayerNum);%!!!X's row must be data  
    X=X';%each column is a data
    N=size(X,2);
-   H=fordProp_batchVersion(W,b,X,hiddentLayerNum)  
+  
    S=log(H{end})'.*labels(1:size(H{end},2),:);% each row is cross entropy for a data
    J=ones(1,size(S,1))*S*ones(size(S,2),1);%sum all the term in S
    J=-J/N;
