23de4bafdd8b15b75ea86bfbfb40210a241e7c2c
Xavier-Pan <sypan.cser@gmail.com>
2017-06-02 20:23:38 +0800

    Add files via upload

--- AAE-Adversarial autoencoder/Adversial_autoencoder(AAE).py
+++ AAE-Adversarial autoencoder/Adversial_autoencoder(AAE).py
@@ -72,7 +72,7 @@ biases_disc = {
 # define placeholder for inputs to network
 x_image = tf.placeholder(tf.float32, [None, 28*28])   # 28x28
 y = tf.placeholder(tf.float32, [None])   # for discriminator
-keep_prob = tf.placeholder(tf.float32)
+#keep_prob = tf.placeholder(tf.float32)
 sample = tf.placeholder(tf.float32, [None, 100])#store sample from gaussian
 #================================== autoencoder ===============================
 #================================== (encoder) - generator ===================== 
@@ -87,8 +87,6 @@ encode = encoder(x_image)
 h1_layer_e = tf.nn.relu(tf.matmul(x_image,weights_auto['h1_e'])+biases_auto['b1_e'])
 h2_layer_e = tf.nn.relu(tf.matmul(h1_layer_e,weights_auto['h2_e'])+biases_auto['b2_e'])
 z_layer = tf.nn.relu(tf.matmul(h2_layer_e,weights_auto['h3_e'])+biases_auto['b3_e'])
-
-
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     print(sess.run(encode, feed_dict={x_image: np.reshape(train_data[0],[1,-1])}))#, feed_dict={x_image: batch_xs, keep_prob: 0.5}
@@ -136,13 +134,13 @@ d_loss2 = -g_loss
 d_loss = d_loss1 + d_loss2
 #==================== (training) ==============================================
 train_list_disc = [weights_disc['h1'],weights_disc['h2'],weights_disc['h3'],biases_disc['b1'],biases_disc['b2'],biases_disc['b3']]
-train_list_auto = [weights_auto['h1_e'],weights_auto['h2_e'],weights_auto['h3_e'],             
+train_list_g = [weights_auto['h1_e'],weights_auto['h2_e'],weights_auto['h3_e'],             
                    biases_auto['b1_e'],biases_auto['b2_e'],biases_auto['b3_e']]                 
 
 train_autoencoder = tf.train.AdamOptimizer(1e-4).minimize(loss_autoencoder)
-train_encoder = tf.train.AdamOptimizer(1e-4).minimize(g_loss,var_list = train_list_auto)
+train_encoder = tf.train.AdamOptimizer(1e-4).minimize(g_loss,var_list = train_list_g)
 #train_discriminator_part1 = tf.train.AdamOptimizer(1e-4).minimize(d_loss1)
-train_discriminator = tf.train.AdamOptimizer(1e-6).minimize(d_loss ,var_list = train_list_disc)
+train_discriminator = tf.train.AdamOptimizer(1e-4).minimize(d_loss ,var_list = train_list_disc)
 
 #==============================================================================
 
@@ -171,6 +169,7 @@ saver = tf.train.Saver()#for store argument
 z_code =  np.zeros([batch_size,100])
 z_code_all = np.zeros([train_size,100])
 sample_data = np.zeros([batch_size,100])
+indices = np.random.choice(a=train_size,size=train_size,replace=False)
 #=====================================================
 with tf.Session() as sess:
     sess.run(init)	
@@ -178,16 +177,13 @@ with tf.Session() as sess:
         saver.restore(sess, os.getcwd()+"\\model\\AAE.ckpt")
     else:
         print("can't load the argument")
-    record_loss_auto.append(sess.run(loss_autoencoder, feed_dict={x_image: train_data[:batch_size],sample:sample_data, keep_prob: 0.5}))          
-    record_loss_gen.append(sess.run(g_loss, feed_dict={x_image: train_data[:batch_size],sample:sample_data, keep_prob: 0.5}))          
-    record_loss_disc.append(sess.run(d_loss, feed_dict={x_image: train_data[:batch_size],sample:sample_data, keep_prob: 0.5}))    
+  
     #============= (training) ==========================  
-    for epoch in range(50):
+    for epoch in range(200):
         for i in range(total_batch):
-            #========= get batch data ==================
-            #batch_xs, batch_ys = mnist.train.next_batch(100)
-            #train_data, train_label, valid_data,valid_label, test_data,test_label
-            batch_xs = train_data[i*batch_size:(i+1)*batch_size] 
+            #========= get batch data (shaffle) ==================
+            #batch_xs, batch_ys = mnist.train.next_batch(100)          
+            batch_xs = train_data[indices][i*batch_size:(i+1)*batch_size] 
             #============= training step  =========================
             '''     
             bbb = sess.run(h_conv2, feed_dict={x_image: np.reshape(train_data[0],[1,28,28,3]), keep_prob: 0.5})
@@ -195,17 +191,16 @@ with tf.Session() as sess:
             print("h_conv2_d's shape:{}".format(np.shape(bbb)))
             print("reconstuct_image's shape:{}".format(np.shape(aaa)))
             ''' 
-    
-            sess.run(train_autoencoder, feed_dict={x_image: batch_xs, keep_prob: 0.5})            
-            z_code = sess.run(encode, feed_dict={x_image: batch_xs, keep_prob: 0.5})
+            sess.run(train_autoencoder, feed_dict={x_image: batch_xs})            
+            z_code = sess.run(encode, feed_dict={x_image: batch_xs})
             sample_data = np.random.multivariate_normal(mean = np.zeros([100]), cov = np.identity(100),size = batch_size)
-            sess.run(train_discriminator, feed_dict={x_image: batch_xs,sample:sample_data, keep_prob: 0.5})    
-            sess.run(train_encoder, feed_dict={x_image: batch_xs, keep_prob: 0.5})    
+            sess.run(train_discriminator, feed_dict={x_image: batch_xs,sample:sample_data})    
+            sess.run(train_encoder, feed_dict={x_image: batch_xs})    
             
         #================ (evaluate loss) =====================================          
-        record_loss_auto.append(sess.run(loss_autoencoder, feed_dict={x_image: train_data[:batch_size],sample:sample_data, keep_prob: 0.5}))          
-        record_loss_gen.append(sess.run(-g_loss, feed_dict={x_image: train_data[:batch_size],sample:sample_data, keep_prob: 0.5}))          
-        record_loss_disc.append(sess.run(d_loss, feed_dict={x_image: train_data[:batch_size],sample:sample_data, keep_prob: 0.5}))          
+        record_loss_auto.append(sess.run(loss_autoencoder, feed_dict={x_image: train_data[:batch_size],sample:sample_data}))          
+        record_loss_gen.append(sess.run(-g_loss, feed_dict={x_image: train_data[:batch_size],sample:sample_data}))          
+        record_loss_disc.append(sess.run(d_loss, feed_dict={x_image: train_data[:batch_size],sample:sample_data}))          
         #if i % 100 == 0:        
     #    print("disc===:",sess.run(disc_fortest, feed_dict={x_image: train_data,sample:sample_data, keep_prob: 0.5})[0])
         print("epoch:{0:4d}  Loss_auto:{1:.3f}  Loss_gen:{2:.3f}  Loss_disc:{3:.3f}".format(epoch+1,
@@ -213,7 +208,7 @@ with tf.Session() as sess:
                 record_loss_gen[-1],
                 record_loss_disc[-1]))
     #================================================================  
-    z_code_all = sess.run(encode, feed_dict={x_image: train_data,sample:np.zeros([1,100]), keep_prob: 0.5})#get all image's z code
+    z_code_all = sess.run(encode, feed_dict={x_image: train_data,sample:np.zeros([1,100]) })#get all image's z code
     '''
     from scipy.misc import toimage
     test = sess.run(reconstuct_image, feed_dict={x_image: train_data[:10]/255, keep_prob: 0.5})         
@@ -224,7 +219,7 @@ with tf.Session() as sess:
         plt.subplot(2,5,(i-k)%9+1)# create a grid of 3x3 images
         plt.imshow(toimage([test[i,:h,:w,0],test[i,:h,:w,1],test[i,:h,:w,2]]))#plt.imshow(toimage([tB[i,:h,:w,0],tB[i,:h,:w,1],tB[i,:h,:w,2]]))    
         '''  
-    global_reconstruct_image = sess.run(decoder, feed_dict={x_image: train_data[:10], keep_prob: 0.5})
+    global_reconstruct_image = sess.run(reconstruct_image, feed_dict={x_image: train_data[:10] })
     save_path = saver.save(sess, os.getcwd()+"\\model\\AAE.ckpt")#save augument 
 
 tEnd = time.clock()#record time
@@ -270,23 +265,24 @@ scatter = []
 index = range(10)
 for i in range(10):
     tmp = np.where(train_label == i)
-    scatter.append(plt.scatter(t_z[tmp, 0], t_z[tmp, 1], c = colors[i] ,s = 5))
+    scatter.append(plt.scatter(t_z[tmp, 0], t_z[tmp, 1], c = colors[i] ,s = 10))
 
 plt.legend(scatter, index)
 plt.show()
 #===================================
 #===plot image =====================
+global_reconstruct_image2 = np.reshape(global_reconstruct_image,[-1,28,28])
+np.shape(global_reconstruct_image2)
 from scipy.misc import toimage
 #X_train=np.reshape(data_sets['images_test'], (-1,32, 32,3))
 h= w = 28
 k = 0
 for i in range(k,k+5):
-    plt.subplot(2,5,(i-k)%9+1)# create a grid of 3x3 images
-    plt.imshow(toimage([global_reconstruct_image[i,:h,:w],global_reconstruct_image[i,:h,:w],global_reconstruct_image[i,:h,:w]]))#plt.imshow(toimage([tB[i,:h,:w,0],tB[i,:h,:w,1],tB[i,:h,:w,2]]))    
+    plt.subplot(2,5,i%9+1)# create a grid of 3x3 images
+    plt.imshow(toimage([global_reconstruct_image2[i,:h,:w],global_reconstruct_image2[i,:h,:w],global_reconstruct_image2[i,:h,:w]]))#plt.imshow(toimage([tB[i,:h,:w,0],tB[i,:h,:w,1],tB[i,:h,:w,2]]))    
 #print(np.shape(data_sets['images_test']))
-
+data = np.reshape(train_data[:10],[-1,28,28])
 for i in range(5):
     plt.subplot(2,5,i+1+5)   
-    plt.imshow(toimage([train_data[i,:h,:w,0],train_data[i,:h,:w,1],train_data[i,:h,:w,2]]))
-
-
+    plt.imshow(toimage([data[i,:h,:w],data[i,:h,:w],data[i,:h,:w]]))
+np.shape(data)
